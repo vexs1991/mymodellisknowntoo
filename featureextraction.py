a3e50b01894206c73882230cdc19e2ae84350bff
Manuel <mk@it-admins.net>
2018-03-22 17:44:15 +0100

    sha256

--- featureextraction.py
+++ featureextraction.py
@@ -1,12 +1,14 @@
 from utils import pefeatures, interface
+import hashlib
 
 file_name = "/home/manuel/Downloads/FoxitReader90_enu_Setup_Prom.exe"
 
 fe = pefeatures.PEFeatureExtractor()
-feats = fe.extract(interface.fetch_file(file_name))
+file = interface.fetch_file_withsha256_chunked(file_name)
+feats = fe.extract(file['data'])
 feats_list = feats.tolist()
 
-print("file name = {}, features size: {}, feature_format: {}\n".format(file_name, feats.size, type(feats[0])))
+print("file name = {}, file sha256 = {}, features size: {}, feature_format: {}\n".format(file_name, file['sha256'], feats.size, type(feats[0])))
 
 section_start = 0
 section_end = 0
--- utils/interface.py
+++ utils/interface.py
@@ -1,13 +1,20 @@
 import os
+import hashlib
 
 class FileRetrievalFailure(Exception):
     pass
 
-def fetch_file(file_path):
+def fetch_file_withsha256_chunked(file_path):
+    sha256 = hashlib.sha256()
     try:
         with open(file_path, 'rb') as infile:
-            bytez = infile.read()
+            while True:
+                data = infile.read(65536)
+                if not data:
+                    break
+            sha256.update(data)
     except IOError:
+        print(IOError.strerror)
         raise FileRetrievalFailure(
             "Unable to read file from {}".format(file_path))
-    return bytez
+    return {'data':data, 'sha256':sha256.hexdigest()}
